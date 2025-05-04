693afb0bd03630aa563c4cc5e8c2a1af45c39cab
PhoeniX5971 <moh845195@gmail.com>
2025-05-04 08:31:10 +0300

    added items

--- src/main/java/net/phoenix/modtemp/Modtemp.java
+++ src/main/java/net/phoenix/modtemp/Modtemp.java
@@ -1,6 +1,8 @@
 package net.phoenix.modtemp;
 
 import net.fabricmc.api.ModInitializer;
+import net.phoenix.modtemp.block.ModBlocks;
+import net.phoenix.modtemp.item.ModItems;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,5 +14,7 @@ public class Modtemp implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/phoenix/modtemp/ModtempClient.java
@@ -0,0 +1,10 @@
+package net.phoenix.modtemp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModtempClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+
+}
